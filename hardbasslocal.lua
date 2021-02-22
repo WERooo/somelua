821d432e3696a215851b3987a6b72eda2812945d
WERooo <dwgdehhetdeegr@gmail.com>
2021-02-22 15:55:56 +0300

    Update hardbasslocal.lua

--- hardbasslocal.lua
+++ hardbasslocal.lua
@@ -3,4 +3,8 @@ hook.Add("KeyPress","gaysex",function(ply,key)
 	local tr = ply:GetEyeTrace()
 	local pos = tr.HitPos
 	util.BlastDamage( ply, ply, pos, 69, 100 )
+	local fx = EffectData()
+	fx:SetOrigin(tr.HitPos)
+	fx:SetNormal(tr.HitNormal)
+	util.Effect("Explosion", fx)
 end)
